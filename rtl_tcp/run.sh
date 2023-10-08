4fba80b455b2f5029cdc7e1c80de83e666ac0996
Ondřej Palovský <palovsky.ondrej@gmail.com>
2023-10-08 20:46:17 +0200

    u

--- rtl_tcp/run.sh
+++ rtl_tcp/run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/with-contenv bashio
 
-echo "Hello world! 2"
+echo "Hello world! 3"
 
 docker run -it --network host --device /dev/bus/usb/003/002 legacycode/rtlsdr rtl_tcp -a 192.168.0.100 -p 1234
